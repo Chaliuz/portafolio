a890dead6df79b162c45e6dece222077e78cba16
Chalius <gonzalo.q.fz@gmail.com>
2021-07-08 23:54:43 -0500

    .gitlab-ci.yml updated to use with react7

--- src/containers/Skills.jsx
+++ src/containers/Skills.jsx
@@ -45,7 +45,7 @@ const Skills = () => {
                           className=" bg-white rounded-circle mb--1 img-center img-fluid shadow-lg "
                           // data-icon={skill.image}
                           src={skill.image}
-                          alt="imageng
+                          alt="imageng"
                           // data-inline="false"
                         />
                       )}
