9a0df1b717391743e2e0bc94364fb1565dfd0b85
chaliuz <gonzalo.q.fz@gmail.com>
2023-09-01 16:48:00 -0500

    first commit

---
+++ commands.py
@@ -0,0 +1,100 @@
+from ranger.api.commands import Command
+from plugins.ranger_udisk_menu.mounter import mount
+
+class paste_as_root(Command):
+	def execute(self):
+		if self.fm.do_cut:
+			self.fm.execute_console('shell sudo mv %c .')
+		else:
+			self.fm.execute_console('shell sudo cp -r %c .')
+
+class fzf_select(Command):
+    """
+    :fzf_select
+
+    Find a file using fzf.
+
+    With a prefix argument select only directories.
+
+    See: https://github.com/junegunn/fzf
+    """
+    def execute(self):
+        import subprocess
+        import os.path
+        if self.quantifier:
+            # match only directories
+            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m --reverse --header='Jump to file'"
+        else:
+            # match files and directories
+            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m --reverse --header='Jump to filemap <C-f> fzf_select'"
+        fzf = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
+
+import os
+from ranger.core.loader import CommandLoader
+class extract_here(Command):
+    def execute(self):
+        """ extract selected files to current directory."""
+        cwd = self.fm.thisdir
+        marked_files = tuple(cwd.get_selection())
+
+        def refresh(_):
+            cwd = self.fm.get_directory(original_path)
+            cwd.load_content()
+
+        one_file = marked_files[0]
+        cwd = self.fm.thisdir
+        original_path = cwd.path
+        au_flags = ['-x', cwd.path]
+        au_flags += self.line.split()[1:]
+        au_flags += ['-e']
+
+        self.fm.copy_buffer.clear()
+        self.fm.cut_buffer = False
+        if len(marked_files) == 1:
+            descr = "extracting: " + os.path.basename(one_file.path)
+        else:
+            descr = "extracting files from: " + os.path.basename(
+                one_file.dirname)
+        obj = CommandLoader(args=['aunpack'] + au_flags
+                            + [f.path for f in marked_files], descr=descr,
+                            read=True)
+
+        obj.signal_bind('after', refresh)
+        self.fm.loader.add(obj)
+
+
+# Chalius commands:
+class extract_audio(Command):
+    """
+    :extract_audio
+
+    Extract audio from current video file with ffmpeg.
+    """
+    def execute(self):
+        import subprocess
+        this_file = self.fm.thisfile
+        command="ffmpeg -i "+ this_file.path +" -acodec libmp3lame audio.mp3"
+
+        self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
+
+class flip_horizontally(Command):
+    """
+    :flip_horizontally
+
+    Flip current video horizontally.
+    """
+    def execute(self):
+        import subprocess
+        this_file = self.fm.thisfile
+        command="ffmpeg -i " + this_file.path + "  -vf hflip -c:a copy flipped.mp4"
+
+        self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
